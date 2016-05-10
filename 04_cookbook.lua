715fc24f15904e974ce0f1343a61e8a6aa57a04b
Hugo Roque <hugolnx@gmail.com>
2016-05-10 06:54:33 -0300

    Uniformiza tabulação

--- 04_cookbook.lua
+++ 04_cookbook.lua
@@ -1,8 +1,8 @@
 --[[
-	Solução no estilo Cookbook, descrito no capítulo 4.
+  Solução no estilo Cookbook, descrito no capítulo 4.
 
-	Este estilo de programação utiliza-se de procedimentos que são executados sequencialmente alterando
-	um estado compartilhado entre eles de maneira que ao final tenhamos as informações desejadas.
+  Este estilo de programação utiliza-se de procedimentos que são executados sequencialmente alterando
+  um estado compartilhado entre eles de maneira que ao final tenhamos as informações desejadas.
 ]]
 
 local text = ""
@@ -11,117 +11,117 @@ local word_frequencies = {}
 
 --[[ Módulo responsável por preparar o texto para análise de frequência. ]]
 filter_text = (function()
-	local function filter_chars()
-		-- filtra o texto retirando os caracters Return , virgula e ponto
-		-- pré-condição o texto extraído tem tamanho maior que 0
-		-- pós-condição o texto não contenha return, vigula ou pontos
-		text = text:gsub("[%W_]", " ")
-	end
+  local function filter_chars()
+    -- filtra o texto retirando os caracters Return , virgula e ponto
+    -- pré-condição o texto extraído tem tamanho maior que 0
+    -- pós-condição o texto não contenha return, vigula ou pontos
+    text = text:gsub("[%W_]", " ")
+  end
 
-	local function normalize()
-		-- transforma todos as letras em minusculas
-		-- pré-condição o texto de entrada tenha tamanho maior que 0 
-		-- pós-condição o texto não contem letras maiusculas
-		text = string.lower(text)
-	end
+  local function normalize()
+    -- transforma todos as letras em minusculas
+    -- pré-condição o texto de entrada tenha tamanho maior que 0 
+    -- pós-condição o texto não contem letras maiusculas
+    text = string.lower(text)
+  end
 
-	local function scan()
-		-- Divide o texto em um vetor de palavras
-		-- pré-condição: um texto filtrado e lowercase, porém em única string
-		-- pós-condição, uma lista de palavras que compõem o texto.
-		for word in text:gmatch("%S+") do
-			words[#words+1] = word
-		end
-	 end
+  local function scan()
+    -- Divide o texto em um vetor de palavras
+    -- pré-condição: um texto filtrado e lowercase, porém em única string
+    -- pós-condição, uma lista de palavras que compõem o texto.
+    for word in text:gmatch("%S+") do
+      words[#words+1] = word
+    end
+   end
 
-	local function remove_stop_words()
-		-- recebe uma table de strings e retorna uma com as stop words removidas
-		-- pré-condição a table tem tamanho maior que 0
-		-- pós-condição nenhum valor da table está na lista de stop_words
-		local file = io.open("input/stop_words.txt")
-		local stop_words_text = file:read("*all")
-		for stop_word in string.gmatch(stop_words_text, "[^,]+") do
-			for i=1, #words do
-				if words[i] == stop_word then
-					words[i] = ""
-				end
-			end
-		end
+  local function remove_stop_words()
+    -- recebe uma table de strings e retorna uma com as stop words removidas
+    -- pré-condição a table tem tamanho maior que 0
+    -- pós-condição nenhum valor da table está na lista de stop_words
+    local file = io.open("input/stop_words.txt")
+    local stop_words_text = file:read("*all")
+    for stop_word in string.gmatch(stop_words_text, "[^,]+") do
+      for i=1, #words do
+        if words[i] == stop_word then
+          words[i] = ""
+        end
+      end
+    end
 
-		for i=1, #words do
-			if #words[i] == 1 then
-				words[i] = nil
-			end
-		end
+    for i=1, #words do
+      if #words[i] == 1 then
+        words[i] = nil
+      end
+    end
 
-		local new_words = {}
-		for i,word in pairs(words) do
-			if word ~= "" then
-				new_words[#new_words+1] = word
-			end
-		end
-		words = new_words
-	end
+    local new_words = {}
+    for i,word in pairs(words) do
+      if word ~= "" then
+        new_words[#new_words+1] = word
+      end
+    end
+    words = new_words
+  end
 
-	return function()
+  return function()
     -- Filtra o texto, tornando-o pronto para a análise de frequência
     -- pré condição: texto cru
     -- pós-condição: texto filtrado. Pronto para a análise de frequência
-		filter_chars()
-		normalize()
-		scan()
-		remove_stop_words()
-	end
+    filter_chars()
+    normalize()
+    scan()
+    remove_stop_words()
+  end
 end)()
 
 function read_file()
-	-- lê o arquivo que contém o texto alvo da aplicação
-	-- pré-condição a abertura do arquivo está correta
-	-- pós-condição o texto extraído tem tamanho maior que 0
-	local file = io.open("input/text.txt")
-	text = file:read("*all")
+  -- lê o arquivo que contém o texto alvo da aplicação
+  -- pré-condição a abertura do arquivo está correta
+  -- pós-condição o texto extraído tem tamanho maior que 0
+  local file = io.open("input/text.txt")
+  text = file:read("*all")
 end
 
 function count_frequencies()
-	-- recebe uma table de indices numeros e valores de strings e retorna uma 
-	--  com indices de string e valores representando a frequencia de sua ocorrencia 
-	--  na table recebida
-	-- pré-condição a table tem tamanho maior que 0
-	-- pós-condição a table tem tamanho maior que 0, apenas com valores positivos
-	local frequency_hash = {}
-	for i,word in ipairs(words) do
-		if frequency_hash[word] then
-			frequency_hash[word] = frequency_hash[word] + 1
-		else
-			frequency_hash[word] = 1
-		end
-	end
+  -- recebe uma table de indices numeros e valores de strings e retorna uma 
+  --  com indices de string e valores representando a frequencia de sua ocorrencia 
+  --  na table recebida
+  -- pré-condição a table tem tamanho maior que 0
+  -- pós-condição a table tem tamanho maior que 0, apenas com valores positivos
+  local frequency_hash = {}
+  for i,word in ipairs(words) do
+    if frequency_hash[word] then
+      frequency_hash[word] = frequency_hash[word] + 1
+    else
+      frequency_hash[word] = 1
+    end
+  end
 
-	for word, frequency in pairs(frequency_hash) do
-		word_frequencies[#word_frequencies+1] = {word, frequency}
-	end
+  for word, frequency in pairs(frequency_hash) do
+    word_frequencies[#word_frequencies+1] = {word, frequency}
+  end
 end
 
 function sort()
-	-- recebe uma table com indices de string e valores representando a frequencia de 
-	--  sua ocorrencia e retorna uma table de chaves numericas com as strings e sua
-	--  frequencia como valor, ordenada por ordem alfabetica
-	-- pré-condição a table tem tamanho maior que 0, apenas com valores positivos
-	-- pós-condição a tabela esta ordenada pela string que esta no segundo valor da tupla
-	table.sort(word_frequencies, function(word_frequency1, word_frequency2)
-		return word_frequency1[2] > word_frequency2[2]
-	end)
+  -- recebe uma table com indices de string e valores representando a frequencia de 
+  --  sua ocorrencia e retorna uma table de chaves numericas com as strings e sua
+  --  frequencia como valor, ordenada por ordem alfabetica
+  -- pré-condição a table tem tamanho maior que 0, apenas com valores positivos
+  -- pós-condição a tabela esta ordenada pela string que esta no segundo valor da tupla
+  table.sort(word_frequencies, function(word_frequency1, word_frequency2)
+    return word_frequency1[2] > word_frequency2[2]
+  end)
 end
 
 function print_text()
-	-- recebe uma table de chaves numericas com as strings e sua frequencia como
-	--  valor, ordenada por ordem alfabetica e imprime na tela as tuplas palavra
-	--  frequencia
-	-- pré-condição a table tem tamanho maior que 0 e esta ordenada por ordem alfabetica das strings
-	-- pós-condição as strings sao impressas na tela com sua frequencia
-	for i=1, math.min(#word_frequencies, 25)  do
-		print(word_frequencies[i][1] .. " " .. word_frequencies[i][2]);
-	end
+  -- recebe uma table de chaves numericas com as strings e sua frequencia como
+  --  valor, ordenada por ordem alfabetica e imprime na tela as tuplas palavra
+  --  frequencia
+  -- pré-condição a table tem tamanho maior que 0 e esta ordenada por ordem alfabetica das strings
+  -- pós-condição as strings sao impressas na tela com sua frequencia
+  for i=1, math.min(#word_frequencies, 25)  do
+    print(word_frequencies[i][1] .. " " .. word_frequencies[i][2]);
+  end
 end
 
 read_file()
--- 09_the_one.lua
+++ 09_the_one.lua
@@ -12,19 +12,18 @@ TFTheOne = { value=0 }
 
 -- função que liga as funções externas para um funcionamento em cascata
  function TFTheOne.bind(self,func)
-        self.value = func(self.value)
-        return self
+	self.value = func(self.value)
+	return self
  end
 
 -- função que exporta o resultado final do processamento
  function TFTheOne.printme(self)
-
-    -- exporta o resultado de forma visível ao usuário da aplicação
-    -- pré-cond: array de 25 strings no formato palavra - frequencia
-    -- pós-cond: valores do array devidamente printados no console
-    for i=1 , #self.value do
-        print(self.value[i] ) 
-    end
+	-- exporta o resultado de forma visível ao usuário da aplicação
+	-- pré-cond: array de 25 strings no formato palavra - frequencia
+	-- pós-cond: valores do array devidamente printados no console
+	for i=1 , #self.value do
+			print(self.value[i] ) 
+	end
  end
 
 --  
@@ -32,120 +31,120 @@ TFTheOne = { value=0 }
 -- 
 
 function read_file(path_to_file)
-    -- lê o arquivo que contém o texto alvo da aplicação
-    -- pré-condição a abertura do arquivo está correta
-    -- pós-condição o texto extraído tem tamanho maior que 0
-    local file = io.open(path_to_file, "r")
-    local data = file:read("*all")
-    return data
+	-- lê o arquivo que contém o texto alvo da aplicação
+	-- pré-condição a abertura do arquivo está correta
+	-- pós-condição o texto extraído tem tamanho maior que 0
+	local file = io.open(path_to_file, "r")
+	local data = file:read("*all")
+	return data
 end
 
 function filter_text(text)
-    -- Filtra o texto, tornando-o pronto para a análise de frequência
-    -- pré condição: texto cru
-    -- pós-condição: texto filtrado. Pronto para a análise de frequência
-    local text = filter_chars(text)
-    text = normalize(text)
-    local words = scan(text)
-    local new_words = remove_stop_words(words)
-    return new_words
-
+	-- Filtra o texto, tornando-o pronto para a análise de frequência
+	-- pré condição: texto cru
+	-- pós-condição: texto filtrado. Pronto para a análise de frequência
+	local text = filter_chars(text)
+	text = normalize(text)
+	local words = scan(text)
+	local new_words = remove_stop_words(words)
+	return new_words
 end
 
 function filter_chars(text)
-    -- filtra o texto retirando os Carriage Return , virgulas e pontos
-    -- pré condição: texto cru
-    -- pós-condição: texto filtrado. Sem caracteres especiais.
-    local text = text:gsub("[%W_]", " ")
-    return text
+	-- filtra o texto retirando os Carriage Return , virgulas e pontos
+	-- pré condição: texto cru
+	-- pós-condição: texto filtrado. Sem caracteres especiais.
+	local text = text:gsub("[%W_]", " ")
+	return text
 end
 
 
 function normalize(text)
-    -- Normaliza a string para problemas de sensitividade de caixa
-    -- pré condição: texto filtrado porém sensível à caixa
-    -- pós-condição: texto filtrado, em lowercase.
-    return string.lower(text)
+	-- Normaliza a string para problemas de sensitividade de caixa
+	-- pré condição: texto filtrado porém sensível à caixa
+	-- pós-condição: texto filtrado, em lowercase.
+	return string.lower(text)
 end
 
 function scan(text)
-    -- Divide o texto em uma lista de palavras
-    -- pré-condição: um texto filtrado e lowercase, porém em única string
-    -- pós-condição, uma lista de palavras que compõem o texto.
-    local words = {}
-    for word in text:gmatch("%S+") do
-        words[#words+1] = word
-    end
-    return words
+	-- Divide o texto em uma lista de palavras
+	-- pré-condição: um texto filtrado e lowercase, porém em única string
+	-- pós-condição, uma lista de palavras que compõem o texto.
+	local words = {}
+	for word in text:gmatch("%S+") do
+		words[#words+1] = word
+	end
+	return words
 end
 
 function remove_stop_words(words)
-    -- remove stop words do texto
-    -- pré-condição: texto filtrado e table de stop_words
-    -- pós-condição: texto com palavras removidas 
-    local file = io.open('input/stop_words.txt', "r") 
-    local stop_words = file:read("*all")
-    
+	-- remove stop words do texto
+	-- pré-condição: texto filtrado e table de stop_words
+	-- pós-condição: texto com palavras removidas 
+	local file = io.open('input/stop_words.txt', "r") 
+	local stop_words = file:read("*all")
+	
 
-    for stop_word in string.gmatch(stop_words, "[^,]+") do
-        for i=1, #words do
-            if words[i] == stop_word then
-                words[i] = ""
-            end
-        end
-    end
+	for stop_word in string.gmatch(stop_words, "[^,]+") do
+		for i=1, #words do
+			if words[i] == stop_word then
+				words[i] = ""
+			end
+		end
+	end
 
-    for i=1, #words do
-        if #words[i] == 1 then
-            words[i] = nil
-        end
-    end
+	for i=1, #words do
+		if #words[i] == 1 then
+			words[i] = nil
+		end
+	end
 
-    local new_words = {}
-    for i,word in pairs(words) do
-        if word ~= "" then
-            new_words[#new_words+1] = word
-        end
-    end
-    return new_words
+	local new_words = {}
+	for i,word in pairs(words) do
+		if word ~= "" then
+			new_words[#new_words+1] = word
+		end
+	end
+	return new_words
 end
-function count_frequencies(words)
-    -- conta a quantidade que o texto aparece na busca
-    -- pré-cond: texto sem stop words
-    -- pós-cond: palavras contadas num array associativo (key, frequencia)
-   local frequency_hash = {}
-   local word_frequencies = {}
-    for i,word in ipairs(words) do
-        if frequency_hash[word] then
-            frequency_hash[word] = frequency_hash[word] + 1
-        else
-            frequency_hash[word] = 1
-        end
-    end
 
-    for word, frequency in pairs(frequency_hash) do
-        word_frequencies[#word_frequencies+1] = {word, frequency}
-    end
-    return word_frequencies
+function count_frequencies(words)
+	-- conta a quantidade que o texto aparece na busca
+	-- pré-cond: texto sem stop words
+	-- pós-cond: palavras contadas num array associativo (key, frequencia)
+	local frequency_hash = {}
+	local word_frequencies = {}
+	for i,word in ipairs(words) do
+		if frequency_hash[word] then
+			frequency_hash[word] = frequency_hash[word] + 1
+		else
+			frequency_hash[word] = 1
+		end
+	end
+
+	for word, frequency in pairs(frequency_hash) do
+		word_frequencies[#word_frequencies+1] = {word, frequency}
+	end
+	return word_frequencies
 end
 function sort(word_frequencies)
-    -- conta a quantidade que o texto aparece na busca
-    -- pré-cond: texto sem stop words
-    -- pós-cond: palavras contadas num array associativo (key, frequencia)
-     table.sort(word_frequencies, function(word_frequency1, word_frequency2)
-        return word_frequency1[2] > word_frequency2[2]
-    end)
-     return word_frequencies
+	-- conta a quantidade que o texto aparece na busca
+	-- pré-cond: texto sem stop words
+	-- pós-cond: palavras contadas num array associativo (key, frequencia)
+	table.sort(word_frequencies, function(word_frequency1, word_frequency2)
+		return word_frequency1[2] > word_frequency2[2]
+	end)
+	return word_frequencies
 end
 function top25_freqs(word_frequencies)
-    -- consegue as 25 palavras mais frequentes
-    -- pré-cond: array associativo (key,frequencia) ordenado
-    -- pós-cond: array de 25 strings no formato palavra - frequencia
-    top25 = {}
-    for i=1, math.min(#word_frequencies, 25)  do
-        top25[i] = word_frequencies[i][1] .. " - " .. word_frequencies[i][2]
-    end
-    return top25
+	-- consegue as 25 palavras mais frequentes
+	-- pré-cond: array associativo (key,frequencia) ordenado
+	-- pós-cond: array de 25 strings no formato palavra - frequencia
+	top25 = {}
+	for i=1, math.min(#word_frequencies, 25)  do
+		top25[i] = word_frequencies[i][1] .. " - " .. word_frequencies[i][2]
+	end
+	return top25
 end
 
 -- 
@@ -153,10 +152,10 @@ end
 --  A função principal (main)
 -- 
 
-    TFTheOne.value = "input/text.txt"
-    TFTheOne:bind(read_file)
-    TFTheOne:bind(filter_text)
-    TFTheOne:bind(count_frequencies)
-    TFTheOne:bind(sort)
-    TFTheOne:bind(top25_freqs)
-    TFTheOne:printme()
+	TFTheOne.value = "input/text.txt"
+	TFTheOne:bind(read_file)
+	TFTheOne:bind(filter_text)
+	TFTheOne:bind(count_frequencies)
+	TFTheOne:bind(sort)
+	TFTheOne:bind(top25_freqs)
+	TFTheOne:printme()
